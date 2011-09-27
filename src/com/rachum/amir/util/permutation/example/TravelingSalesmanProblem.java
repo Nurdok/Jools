d290016c564c1566abc25c2568594b72c4b7426b
nurdok <nurdok@gmail.com>
2011-09-27 21:50:30 +0300

    bla

--- src/com/rachum/amir/util/permutation/example/TravelingSalesmanProblem.java
+++ src/com/rachum/amir/util/permutation/example/TravelingSalesmanProblem.java
@@ -19,7 +19,7 @@ public class TravelingSalesmanProblem {
 	
 	private final Map<String, Point> citiesCoord;
 
-	public TravelingSalesmanProblem(final int numOfCities) {
+	public TravelingSalesmanProblem() {
 		super();
 		citiesCoord = new HashMap<String, Point>();
 		citiesCoord.put("A", new Point(0,0));
@@ -28,32 +28,48 @@ public class TravelingSalesmanProblem {
 		citiesCoord.put("D", new Point(2,10));
 		citiesCoord.put("E", new Point(2,3));
 	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(final String[] args) {
-		//List<String> route;
-		final TravelingSalesmanProblem tsp = new TravelingSalesmanProblem(10);
+	
+	private int calculateRouteLength(final List<String> route) {
+		return 0;
+	}
+	
+	public List<String> naiveSolve() {
+		int minRouteLength = Integer.MAX_VALUE;
+		List<String> minRoute = null;
+		final PermutationGenerator<String> routeGenerator =
+			new PermutationGenerator<String>(new ArrayList<String>(citiesCoord.keySet()));
+		
+		for (final List<String> route : routeGenerator) {
+			final int routeLength = calculateRouteLength(route);
+			if (routeLength < minRouteLength) {
+				minRouteLength = routeLength;
+				minRoute = route;
+			}
+		}
+		return minRoute;
+	}
+	
+	
+	public List<String> thresholdSolve() {
 		final PermutationGenerator<String> routeGenerator =
 			//new PermutationGenerator<String>(Arrays.asList("A","B","C","D","E"));
-			new PermutationGenerator<String>(new ArrayList<String>(tsp.citiesCoord.keySet()));
+			new PermutationGenerator<String>(new ArrayList<String>(citiesCoord.keySet()));
 		
 		int threshold = Integer.MAX_VALUE;
 		List<String> bestRoute = routeGenerator.get(0);
 		boolean skip = false;
 		int routesChecked = 0;
-		for (int i=0; i<routeGenerator.size()/tsp.citiesCoord.size(); ++i, ++routesChecked) {
+		for (int i=0; i<routeGenerator.size()/citiesCoord.size(); ++i, ++routesChecked) {
 			final List<String> route = routeGenerator.get(i);
 			int length = 0;
 			skip = false;
 			for (int j=0; j<route.size(); ++j) {
-				final Point p1 = tsp.citiesCoord.get(route.get(j));
+				final Point p1 = citiesCoord.get(route.get(j));
 				Point p2 = null;
 				if (j == 0) {
-					p2 = tsp.citiesCoord.get(route.get(route.size()-1));
+					p2 = citiesCoord.get(route.get(route.size()-1));
 				} else {
-					p2 = tsp.citiesCoord.get(route.get(j-1));
+					p2 = citiesCoord.get(route.get(j-1));
 				}
 				length += p1.distance(p2);
 				
@@ -71,7 +87,18 @@ public class TravelingSalesmanProblem {
 			}
 		}
 		System.out.println("Best route is " + bestRoute + ", length is " + threshold);
-		System.out.println("Checked " + routesChecked + " out of " + routeGenerator.size()/tsp.citiesCoord.size() + " possible routes.");
+		System.out.println("Checked " + routesChecked + " out of " + routeGenerator.size()/citiesCoord.size() + " possible routes.");
+		return bestRoute;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		//List<String> route;
+		final TravelingSalesmanProblem tsp = new TravelingSalesmanProblem();
+		System.out.println("Naive solution: " + tsp.naiveSolve());
+		System.out.println("Threshold solution: " + tsp.thresholdSolve());
 	}
 
 }
